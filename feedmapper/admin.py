9c957211169df37108c548d28abdf46c16706db1
Hannes Tismer <hannes.tismer@paperc.com>
2014-07-23 15:28:36 +0200

    support running a mapping directly from admin

--- feedmapper/admin.py
+++ feedmapper/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
 from .models import Mapping
+from .tasks import feedmapper_sync
+
+
+def run_mapping(modeladmin, request, queryset):
+    """
+    Initiates the `benchmarking` (TODO: ref required) of the selected epubs.
+    """
+    for mapping in queryset.all():
+        feedmapper_sync.delay(mapping.id)
+
+
+run_mapping.short_description = "Run selected mappings"
 
 
 class MappingAdmin(admin.ModelAdmin):
@@ -16,5 +28,7 @@ class MappingAdmin(admin.ModelAdmin):
     list_display = ('label', 'parser', 'purge', 'parse_attempted', 'parse_succeeded')
     list_filter = ('parser', 'parse_succeeded')
     readonly_fields = ('parse_log',)
+
+    actions = [run_mapping]
 
 admin.site.register(Mapping, MappingAdmin)
